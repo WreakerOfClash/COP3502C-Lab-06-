ee45f25a0054f34413b953062e226fb8d1495cc8
jen <jenniferklaman@gmail.com>
2023-03-22 17:00:43 -0400

    decode added!!!!!!

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def encode(original_pass):
         enc_pass += string
     return enc_pass
 
+
 def decode(password):
     password_lst = []
     decoded_password = ""
@@ -26,3 +27,36 @@ def decode(password):
 
     except TypeError as exc:
         print(exc)
+
+
+def print_menu():
+    print("Menu\n",
+          "-------------\n",
+          "1. Encode\n",
+          "2. Decode\n",
+          "3. Quit\n")
+
+
+def main():
+    quit_program = False
+
+    while not quit_program:
+
+        print_menu()
+
+        menu_select = int(input("Please enter an option: "))
+
+        if menu_select == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif menu_select == 2:
+            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
+        elif menu_select == 3:
+            quit_program = True
+
+
+if __name__ == "__main__":
+    main()
